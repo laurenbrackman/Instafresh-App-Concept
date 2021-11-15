8ef774166c13f5d548e18edf8c6ab879be3d9188
Lauren Helbling <laurengraceh97@gmail.com>
2021-11-15 13:14:20 -0600

    Fix no ingredients

--- js/scripts.js
+++ js/scripts.js
@@ -70,14 +70,15 @@ function populate(arr, names){
     let recipes = document.getElementsByClassName('col-sm');
     let counter = 0;
     for (let recipe of recipes){
-        arrayItem = arr[counter];
+        let arrayItem = arr[counter];
         let name = names[counter];
         let parsedName = arrayItem['parsedName'];
         let subcaption = arrayItem['subcaption'];
         let description = arrayItem['description'];
+        console.log(`Ingredients for ${parsedName} = ${Object.keys(arrayItem['ingredients'])}`);
         recipe.innerHTML = `<img src='img/${name}.jpg'></img><h3>${parsedName}</h3><h5>${subcaption}</h5><p>${description}</p>`;
         let selected = false;
-        recipe.onclick= function(){
+        recipe.addEventListener('click', function(){
             if(selected){
                 selected = false;
                 removeGroceries(arrayItem['ingredients']);
@@ -88,32 +89,42 @@ function populate(arr, names){
                 addGroceries(arrayItem['ingredients']);
                 recipe.innerHTML = `<img src='img/${name}-selected.png'></img><h3>${parsedName}</h3><h5>${subcaption}</h5><p>${description}</p>`;
             }
-        }   
+        })   
         counter += 1;
     };
 }
 
-
 //Generate Grocery List
-let generateButton = document.getElementById('generate');
 let groceries = {"No ingredients": '0'};
+let generateButton = document.getElementById('generate');
 generateButton.addEventListener('click', () => {displayGroceries();});
 
 function addGroceries(recipe){
-    console.log("Adding " + recipe);
+    if(groceries["No ingredients"]){
+        delete groceries["No ingredients"];
+    }
+    console.log("Adding " + Object.keys(recipe));
     for(ingredient in recipe){
         groceries[ingredient]=recipe[ingredient];
     }
 }
 
 function removeGroceries(recipe){
-    console.log("Removing " + recipe);
+    console.log("Removing " + Object.keys(recipe));
     for(ingredient in recipe){
         delete groceries[ingredient];
     }
+    if(Object.keys(groceries).length == 0){
+        groceries['No ingredients'] = '0';
+    }
+}
+
+function clearList(){
+    groceryList.innerHTML = "";
 }
 
 function displayGroceries(){
+    clearList();
     groceryList = document.getElementById('groceryList');
     groceryListArray = Object.keys(groceries);
     for(ingredient in groceryListArray){
